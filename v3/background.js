31f4a98cb6569f8de343489ddb0c734a85e8b1ed
Peter Havekes <peter.havekes@surfnet.nl>
2020-12-29 11:58:10 +0100

    Version working in TB82+

--- v3/background.js
+++ v3/background.js
@@ -1,7 +1,6 @@
+console.log('Loaded');
 let registeredScripts = browser.messageDisplayScripts.register({
   js: [
-    // Any number of code or file objects could be listed here.
-    { code: `document.body.textContent = "Hey look, the script ran!";` },
-    { file: "/display.js" },
+    { file: "display.js" },
   ],
 });
